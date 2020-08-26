b5ba13ecb079b56860b24fd0695ccc713c6e0ff5
Keshari Nandan <keshari.nandan2@gmail.com>
2020-08-26 09:36:00 +0530

    Added grid helpers

---
+++ src/Build/Helpers/DateRangeFilter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TheNandan\Grids\Build\Helpers;
+
+use TheNandan\Grids\Components\Filters\DateRangePicker;
+
+class DateRangeFilter
+{
+    protected $filter;
+    protected $options;
+    protected $isRelated = false;
+
+    public function __construct($name)
+    {
+        $this->filter = new DateRangePicker();
+        $this->filter->setName($name)->setRenderSection('filters_row_column_'.$name)->setDefaultValue([null, null]);
+    }
+
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+
+    public function addOption($key, $value)
+    {
+        $this->options[$key] = $value;
+        $this->filter->setJsOptions($this->options);
+    }
+
+    public function setFilteringFunc($function)
+    {
+        $this->filter->setFilteringFunc($function);
+    }
+}
---
+++ src/Build/Helpers/SelectFilter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TheNandan\Grids\Build\Helpers;
+
+use TheNandan\Grids\SelectFilterConfig;
+use TheNandan\Grids\EloquentDataProvider;
+
+class SelectFilter extends SearchFilter
+{
+    public function __construct($name, $options)
+    {
+        $this->filter = (new SelectFilterConfig())->setOptions($options);
+        $this->filter->setName($name);
+        parent::__construct($name, $options);
+    }
+
+    public function setDefaultFilteringFunc($name, $relation = false)
+    {
+        $this->filter->setFilteringFunc(function ($val, EloquentDataProvider $dp) use ($relation, $name) {
+            $builder = $dp->getBuilder();
+            if ($relation) {
+                $builder->whereHas($relation, function ($query) use ($val, $name) {
+                    $query->where($name, $val);
+                });
+            } else {
+                $builder->where($name, $val);
+            }
+        });
+    }
+
+    public function setOptions(array $options){
+        $this->filter->setOptions($options);
+    }
+}
