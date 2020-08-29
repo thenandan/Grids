d9e65e87071bd19131890b4a6517b8e4fbdafd61
Keshari Nandan <keshari.nandan2@gmail.com>
2020-08-29 22:01:48 +0530

    Added export and other grid utilities

--- src/TheNandanGrid.php
+++ src/TheNandanGrid.php
@@ -124,12 +124,154 @@ class TheNandanGrid
 
     }
 
+    /**
+     * @param array|null $components
+     * @return mixed
+     */
+    private function getFiltersRow(array $components = null)
+    {
+        $filtersRow = $this->gridConfig->getComponentByNameRecursive(FiltersRow::NAME);
+
+        if (!empty($components)) {
+            $filtersRow->addComponents($components);
+        }
+
+        return $filtersRow;
+    }
+
+    /**
+     * @param array|int[] $recordsPerPage
+     *
+     * @return RecordsPerPage
+     */
+    public function setRecordsPerPage(array $recordsPerPage = [10, 20, 50, 100, 200]): RecordsPerPage
+    {
+        return (new RecordsPerPage())->setVariants($recordsPerPage);
+    }
+
+    /**
+     * @param $fileName
+     *
+     * @return ExcelExport
+     */
+    public function setExcelExport($fileName): ExcelExport
+    {
+        return (new ExcelExport())->setFileName($fileName);
+    }
+
+    /**
+     * @param array $attributes
+     * @param $component
+     *
+     * @return HtmlTag
+     */
+    public function setHtmlTag(array $attributes, $component): HtmlTag
+    {
+        return (new HtmlTag())
+            ->setAttributes($attributes)
+            ->addComponent($component);
+    }
+
+    /**
+     * @return HtmlTag
+     */
+    private function setResetFiltersButton(): HtmlTag
+    {
+        return (new HtmlTag())
+            ->setContent('<i class="fas fa-filter"></i> Reset')
+            ->setTagName('button')
+            ->setRenderSection(RenderableRegistry::SECTION_END)
+            ->setAttributes([
+                'type' => 'button',
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'bottom',
+                'title' => 'Reset Filters',
+                'class' => 'btn float-right m-1 btn-success btn-sm clear_filters',
+            ]);
+    }
+
+    /**
+     * @param array $components
+     * @return OneCellRow
+     */
+    private function getOneCellRow(array $components): OneCellRow
+    {
+        return (new OneCellRow())->setComponents($components);
+    }
+
+    /**
+     * @param array $components
+     * @return THead
+     */
+    private function getThead(array $components): THead
+    {
+        return (new THead())->setComponents($components);
+    }
+
+    /**
+     * @param array $components
+     * @return TFoot
+     */
+    private function getTFoot(array $components): TFoot
+    {
+        return (new TFoot())->setComponents($components);
+    }
+
+    public function getCsvExport($fileName)
+    {
+        return (new CsvExport())->setFileName($fileName);
+    }
+
+
+    /**
+     *
+     */
+    public function setDefaultComponents(): void
+    {
+        $pagination = new Pager();
+        $columnHeadersRow = new ColumnHeadersRow();
+        $filersRow = $this->getFiltersRow();
+        $recordsPerPage = $this->setRecordsPerPage();
+        $showingRecords = new ShowingRecords();
+        $columnsHider = (new ColumnsHider())->setHiddenByDefault($this->hiddenColumns);
+        $exportExcel = $this->setExcelExport('file-'.date('d-m-Y-h-i-s'));
+        $exportCSV = $this->getCsvExport('CSV-' . date('d-m-Y-h-i-s'));
+        $recordsPerPageTag = $this->setHtmlTag(['class' => 'float-left m-1'], $recordsPerPage);
+        $columnsHiderTag = $this->setHtmlTag(['class' => 'float-right m-1'], $columnsHider);
+        $exportCsvTag = $this->setHtmlTag(['class' => 'float-right m-1'], $exportCSV);
+        $exportExcelTag = $this->setHtmlTag(['class' => 'float-right m-1 excel_export'], $exportExcel);
+        $resetFiltersButton = $this->setResetFiltersButton();
+        $resetFiltersButtonTag = $this->setHtmlTag(['class' => 'pull-right'], $resetFiltersButton);
+        $paginationTag = $this->setHtmlTag(['class' => 'float-right'], $pagination);
+        $showingRecordsTag = $this->setHtmlTag(['class' => 'float-left'], $showingRecords);
+        $head[] = $recordsPerPageTag;
+        $head[] = $resetFiltersButtonTag;
+        $head[] = $exportCsvTag;
+        $head[] = $exportExcelTag;
+//        $head[] = $columnsHiderTag;
+        $tfoot[] = $showingRecordsTag;
+        $tfoot[] = $paginationTag;
+        $headOneCellRow = $this->getOneCellRow($head);
+        $footOneCellRow = $this->getOneCellRow($tfoot);
+        $thead = $this->getThead([]);
+        $thead->addComponent($headOneCellRow);
+        if (!empty($this->customHeaderRow)) {
+            $thead->addComponent($this->customHeaderRow);
+        }
+        $thead->addComponent($columnHeadersRow);
+        $thead->addComponent($filersRow);
+        $tfoot = $this->getTFoot([$footOneCellRow]);
+        $this->gridConfig->setComponents([$thead, $tfoot]);
+    }
+
+
 
     /**
      * @return View|string
      */
     public function render()
     {
+        $this->setDefaultComponents();
         $grid = new NayGrid($this->gridConfig);
         return $grid->render();
     }
